caf2b40b07457497445a732d72bf458dbbd5c51e
georgeG <georgkam@gmail.com>
2014-08-28 01:02:37 +0300

    call mhcpan3

--- epitope_pipeline.sh
+++ epitope_pipeline.sh
@@ -5,7 +5,6 @@ prediction_method="NetMHCII-pan-3"
 fasta_file=$1
 allele_file=$2
 output_file=$3
-#netmhciipan=/Users/george/netMHCIIpan-3.0/NetMHCpan
 
 echo "Running compute peptides for file" #$file_no" #>> $LOG_FILE
 
@@ -19,7 +18,7 @@ do
 
   echo "BINDING_TEST $mhc_allele " >>$output_file
 
-  /Users/george/netMHCIIpan-3.0/NetMHCIIpan -f $fasta_file $mhc_allele >>$output_file || exit e
+  NetMHCIIpan3 -f $fasta_file $mhc_allele >>$output_file || exit e
 
 done < $allele_file
 
