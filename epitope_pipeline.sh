02679e9d1729a1d86621bae2a0ac5edc0304ab70
georgeG <georgkam@gmail.com>
2014-08-29 16:38:14 +0300

    predict eptiopes given a fasta file, list of alleles and peptide length

--- epitope_pipeline.sh
+++ epitope_pipeline.sh
@@ -4,22 +4,25 @@ prediction_method="NetMHCII-pan-3"
 
 fasta_file=$1
 allele_file=$2
-output_file=$3
+peptide_len=$3
+output_file=$4
 
-echo "Running compute peptides for file" #$file_no" #>> $LOG_FILE
-
+echo "Predicting class II T-cell epitopes" #$file_no" #>> $LOG_FILE
 
 # function to run MHCII program with different alleles
 run_prediction () {
-
-#Read the allele file
-while read mhc_allele
-do
-
-  echo "BINDING_TEST $mhc_allele " >>$output_file
-
-  NetMHCIIpan3 -f $fasta_file $mhc_allele >>$output_file || exit e
-
-done < $allele_file
-
+  #Read the allele file
+  while read mhc_allele
+    do
+      echo "$mhc_allele"
+      netMHCII-pan-3 -f $fasta_file -a $mhc_allele -length $peptide_len | 
+      sed '/\#/d' | sed '/---/d' |
+      sed '/Pos/d' | sed '/Number/d' | 
+      sed 's/^ *//' | sed 's/[ \t]*$//' |
+      sed 's/<=WB//' | sed 's/<=SB//' |
+      tr -s '[:blank:]' ',' |
+      sed '/^$/d'| sed 's/,$//' >>$output_file
+    done <$allele_file
 }
+
+run_prediction
