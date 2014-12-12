80c316afcaa656c9a2daba18c6b32cbd4f683b1b
georgeG <georgkam@gmail.com>
2014-12-12 12:08:41 +0300

    use usearch to sort, remove the time consuming pairwise comparisons for a direct count of substitutions

--- clustering_pipeline.sh
+++ clustering_pipeline.sh
@@ -22,12 +22,12 @@ seqfile=$1
 
 [ $# -eq 0 ] && { echo "Usage: $0 nucleotide_file"; exit 1;}
 
-identities=(98 96 94 92 90 88 86)
+identities=(98 96 94 92 90 88)
 
 seqfilefullpath=$( cd $(dirname $seqfile); pwd)/$(basename $seqfile)
 
-#sort the input sequence with uclust
-uclust --sort $seqfile --output $seqfile.sorted.fasta
+#sort the input sequence with usearch sortbylength command
+usearch -sortbylength $seqfile --output $seqfile.sorted.fasta
 
 sortedfilefullpath=$( cd $(dirname $seqfile); pwd)/$(basename $seqfile.sorted.fasta)
 
@@ -65,9 +65,9 @@ for id in ${identities[@]}; do
   while read clustr; do
     mkdir cluster_$clustr
     cd cluster_$clustr
-      
+
       echo "writing fasta file for cluster_$clustr"
-      
+
       #write the fasta members for clustr $name
       grep "^${clustr}:" ../seqs_per_cluster.txt | awk -F: '{print $2}' | tr , '\n' >members.txt
       fastagrep -F -X -f members.txt $sortedfilefullpath >cluster_$clustr.fasta
@@ -75,21 +75,25 @@ for id in ${identities[@]}; do
       echo "Translating"
       #translate the DNA file
       ruby ~/Softwares/translate.rb -i cluster_$clustr.fasta -f 1 -o cluster_$clustr.aa.fasta
-     
+
       echo "Aligning sequences"
       #align both DNA and AA sequences
       muscle -in cluster_$clustr.fasta -out cluster_$clustr.aln
       muscle -in cluster_$clustr.aa.fasta -out cluster_$clustr.aa.aln
-      
-      echo "finding mutations from the alignments"
+ 
+     #echo "finding mutations from the alignments"
       
       #find the mutations 
-      ruby ~/Softwares/mutation.rb cluster_$clustr.aln >cluster_$clustr.mutations.txt
+      #this procedure does not scale properly
+      #ruby ~/Softwares/mutation.rb cluster_$clustr.aln >cluster_$clustr.mutations.txt
+
+      #replaced the previous with 
+      echo "Searching for polymorphic sites"
       ruby ~/Code/Ruby/blocks/lib/polymorphic_sites.rb cluster_$clustr.aln >cluster_$clustr.snps.txt
 
       #create a file for region coordinates
       ruby ~/Code/Ruby/blocks/lib/print_hv_positions.rb cluster_$clustr.aln >regions_positions.txt
-      
+
       #split alignment to blocks and print start and stop positions
       bash ~/Code/Bash/splice_alignment.sh regions_positions.txt cluster_$clustr.aln
 
@@ -108,37 +112,36 @@ for id in ${identities[@]}; do
       bash ~/Code/Bash/snp_density.sh regions_positions.txt snps_region4.txt cluster_$clustr.aln >region4_snp_density.txt
       bash ~/Code/Bash/snp_density.sh regions_positions.txt snps_region5.txt cluster_$clustr.aln >region5_snp_density.txt
       bash ~/Code/Bash/snp_density.sh regions_positions.txt snps_region6.txt cluster_$clustr.aln >region6_snp_density.txt
-      
-      echo "listing mutation positions"
-     
+
+     # echo "listing mutation positions"
       #list the positions for each mutation
-      awk -F, '{print $1}' cluster_$clustr.mutations.txt | sort -nrk 1 |
-      uniq  >cluster_$clustr.mutation.positions.txt
+      #awk -F, '{print $1}' cluster_$clustr.mutations.txt | sort -nrk 1 |
+      #uniq  >cluster_$clustr.mutation.positions.txt
 
-      echo "Sanitizing"
-      sed 's/A-G/G-A/g; s/T-C/C-T/g; s/A-T/T-A/g; s/A-C/C-A/g; s/C-G/G-C/g; s/T-G/G-T/g; s/--T/T--/g; s/--G/G--/g; s/--C/C--/g; s/--A/A--/g;' <cluster_$clustr.mutations.txt | 
-      awk -F, '{print $1,$2,$3,$4}' | sort -nrk 1 | uniq >cluster_$clustr.mutations.edited.txt
+      #echo "Sanitizing"
+      #sed 's/A-G/G-A/g; s/T-C/C-T/g; s/A-T/T-A/g; s/A-C/C-A/g; s/C-G/G-C/g; s/T-G/G-T/g; s/--T/T--/g; s/--G/G--/g; s/--C/C--/g; s/--A/A--/g;' <cluster_$clustr.mutations.txt | 
+      #awk -F, '{print $1,$2,$3,$4}' | sort -nrk 1 | uniq >cluster_$clustr.mutations.edited.txt
 
-       echo "Listing mutations per codon position"
-       awk '{print $2,$4}' <cluster_$clustr.mutations.edited.txt | sort | uniq -c >cluster_$clustr.mutations_percodon_position.txt
-       
-       echo "printing dN-dS changes"
+       #echo "Listing mutations per codon position"
+       #awk '{print $2,$4}' <cluster_$clustr.mutations.edited.txt | sort | uniq -c >cluster_$clustr.mutations_percodon_position.txt
+
+       echo "dN-dS substitutions"
        ~/Softwares/selection.rb -i cluster_$clustr.aln -S | awk '{print $4}' | sort | uniq -c >cluster_$clustr.dN_dS.txt
     cd ..
   done <most_clusters.txt
 
-  echo "collect all the mutations for $id identity threshold"
-  find . -type f -name *.mutations.edited.txt | xargs cat | awk '{print $2,$4}'| sort | uniq -c | sed -e 's/^[ \t]*//' >all.mutations.txt
+  echo "collect snps in $id identity threshold"
+  #find . -type f -name *.mutations.edited.txt | xargs cat | awk '{print $2,$4}'| sort | uniq -c | sed -e 's/^[ \t]*//' >all.mutations.txt
   find . -type f -name *.snps.txt | xargs cat | sed -e 's/^[ \t]*//' >all.snps.txt
 
-  echo "collect all snps"
+  echo "collect snps for regions"
   find . -type f -name snps_region1.txt | xargs cat | sed -e 's/^[ \t]*//' >all.region1.snps.txt
   find . -type f -name snps_region2.txt | xargs cat | sed -e 's/^[ \t]*//' >all.region2.snps.txt
   find . -type f -name snps_region3.txt | xargs cat | sed -e 's/^[ \t]*//' >all.region3.snps.txt
   find . -type f -name snps_region4.txt | xargs cat | sed -e 's/^[ \t]*//' >all.region4.snps.txt
   find . -type f -name snps_region5.txt | xargs cat | sed -e 's/^[ \t]*//' >all.region5.snps.txt
 
-  echo "collect snp densities"
+  echo "collect snp densities for regions"
   find . -type f -name region1_snp_density.txt | xargs cat | sed -e 's/^[ \t]*//' >all.region1.snps.density.txt
   find . -type f -name region2_snp_density.txt | xargs cat | sed -e 's/^[ \t]*//' >all.region2.snps.density.txt
   find . -type f -name region3_snp_density.txt | xargs cat | sed -e 's/^[ \t]*//' >all.region3.snps.density.txt
